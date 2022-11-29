91209d1d4d12cf5d86208e049f83d42212f1c945
Willa <willasimard@gmail.com>
2022-11-29 12:18:07 -0600

    imported font, starting textboxes

--- sketch.js
+++ sketch.js
@@ -13,12 +13,32 @@ class Buttons {
     this.buttonH = buttonH;
   }
 
-  display(anyImage) {
+  display(word, size, strokeA, strokeB) {
     if (this.isInside(mouseX, mouseY)) {
-      image(anyImage, this.x, this.y, this.buttonW*1.1, this.buttonH*1.1);
+      fill(0);
+      rectMode(CENTER);
+      stroke(255);
+      strokeWeight(strokeA);
+      rect(this.x, this.y, this.buttonW*1.1, this.buttonH*1.1); 
+      textSize(size*1.1);
+      textFont(pixelFont);
+      fill(255);  
+      strokeWeight(strokeB);
+      textAlign(CENTER, CENTER);
+      text(word, this.x + strokeB, this.y - strokeB);
     }
     else {
-      image(anyImage, this.x, this.y, this.buttonW, this.buttonH);
+      fill(0);
+      rectMode(CENTER);
+      stroke(255);
+      strokeWeight(strokeA);
+      rect(this.x, this.y, this.buttonW, this.buttonH);
+      textSize(size);
+      textFont(pixelFont);
+      fill(255);
+      strokeWeight(strokeB);
+      textAlign(CENTER, CENTER);
+      text(word, this.x + strokeB, this.y - strokeB);
     }
   }
 
@@ -31,16 +51,16 @@ class Buttons {
   }
 }
 
-
-
 let gameState = "startingScreen";
 let playButtonImg;
 let playButton;
 let optButtonImg;
 let optButton;
 let textboxImg;
+let pixelFont;
 
 function preload() {
+  pixelFont = loadFont("assets/font.ttf");
   playButtonImg = loadImage("sprites/play_button.png");
   optButtonImg = loadImage("sprites/opt_button.png");
   textboxImg = loadImage("sprites/textbox.png");
@@ -48,7 +68,8 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  imageMode(CENTER);
+  imageMode(CENTER); 
+  rectMode(CENTER);
 }
 
 function draw() {
@@ -56,29 +77,49 @@ function draw() {
   if (gameState === "startingScreen") {
     startScreen();
   }
+  else if (gameState === "options") {
+    optionsScreen();
+  }
+  else if (gameState === "gameBegins") {
+    startGame();
+  }
 }
 
 function startScreen() {
   playButton = new Buttons(windowWidth/2, windowHeight/2, 350, 130);
-  playButton.display(playButtonImg);
+  playButton.display("PLAY", 90, 15, 5);
   if (mouseIsPressed === true && playButton.isInside(mouseX, mouseY)) {
     gameState = "gameBegins";
   }
   optButton = new Buttons(windowWidth/2, windowHeight/2 + 130, 250, 75);
-  optButton.display(optButtonImg);
+  optButton.display("OPTIONS", 40, 10, 1);
   if (mouseIsPressed === true && optButton.isInside(mouseX, mouseY)) {
     gameState = "options";
   }
+  
 
 }
 
-function createTextbox(character, text) {
-  image(textboxImg, )
+function optionsScreen() {
 
+}
+
+function createTextbox(words, boxW, boxH) {
+  textAlign(LEFT, BASELINE);
+  fill(0);
+  stroke(255);
+  strokeWeight(10);
+  rect(width/2, height - boxH, boxW, boxH);
+  textFont(pixelFont);
+  textSize(30);
+  fill(255);
+  strokeWeight(0.5);
+  text(words, width/2 - boxW/2 + 10*1.4, height - boxH - boxH/2 + 30*1.2);
 }
 
 
 
-function gameBegins() {
+function startGame() {
+  createTextbox(">Testing... ", 600, 200);
   
 }
